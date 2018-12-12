aa6582cddebf773b68bbcc56901af47844917f96
Bringerus <kruchkovevg@gmail.com>
2018-12-12 13:18:45 +0400

    PixelPlexTest

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Main from "./components/Main";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Provider } from "react-redux";
+import reducers from "./reducers/index";
+import { createStore } from "redux";
+
+const store = createStore(reducers);
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-sm  navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        <img
+          width="100"
+          src="https://image.ibb.co/k7cmVT/logo_w.png"
+          alt="Logo"
+        />
+      </a>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href=".">
+              Home link <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="https://pixelplex.io" target="_blank">
+              Pixelplex link
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Header />
+          <Main />
+        </div>
+      </Provider>
     );
   }
 }
