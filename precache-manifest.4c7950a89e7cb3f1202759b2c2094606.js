af5888736c679dc4e6d31052cb849a7563f93858
Bringerus <kruchkovevg@gmail.com>
2018-12-12 15:56:53 +0400

    Updates

---
+++ precache-manifest.4c7950a89e7cb3f1202759b2c2094606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04458c1b0a4db0e084f7",
+    "url": "/TestPixelPlex/static/js/runtime~main.04458c1b.js"
+  },
+  {
+    "revision": "f641b3d0cfee9404e219",
+    "url": "/TestPixelPlex/static/js/main.f641b3d0.chunk.js"
+  },
+  {
+    "revision": "e5966d7a3acab11a7e72",
+    "url": "/TestPixelPlex/static/js/1.e5966d7a.chunk.js"
+  },
+  {
+    "revision": "f641b3d0cfee9404e219",
+    "url": "/TestPixelPlex/static/css/main.6a74785c.chunk.css"
+  },
+  {
+    "revision": "e5966d7a3acab11a7e72",
+    "url": "/TestPixelPlex/static/css/1.47087546.chunk.css"
+  },
+  {
+    "revision": "b3e93426f77d7f6c2d96d6e1594aa439",
+    "url": "/TestPixelPlex/index.html"
+  }
+];
